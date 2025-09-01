d8f307b24fb65baa9790c2481efdfbd1aa5277a1
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2025-09-01 19:19:45 +0300

    feat: prepare for deploy on render.

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+﻿# Base image
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS base
+WORKDIR /app
+EXPOSE 80
+
+# Build stage
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["GameBackend.csproj", "./"]
+RUN dotnet restore "GameBackend.csproj"
+COPY . .
+RUN dotnet build "./GameBackend.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+# Publish stage
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./GameBackend.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+# Final stage
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "GameBackend.dll"]
--- Program.cs
+++ Program.cs
@@ -3,7 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+string connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                          $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                          $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                          $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                          $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};" +
+                          "SSL Mode=Require;Trust Server Certificate=true";
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +27,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
 
 // app.UseHttpsRedirection();
 

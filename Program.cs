4519eed084a45499f3a2c1c0349e71189d799140
Andrey Gritsenko <a.gritsenko87@yandex.ru>
2025-09-01 20:06:24 +0300

    feat: revert local use web api.

--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-string connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
-                          $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
-                          $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
-                          $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
-                          $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};" +
-                          "SSL Mode=Require;Trust Server Certificate=true";
+// string connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+//                           $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+//                           $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+//                           $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+//                           $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};" +
+//                           "SSL Mode=Require;Trust Server Certificate=true";
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+// builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -22,16 +22,21 @@ WebApplication app = builder.Build();
 
 app.MapControllers();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-app.Urls.Add($"http://0.0.0.0:{port}");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+// app.Urls.Add($"http://0.0.0.0:{port}");
 
 // app.UseHttpsRedirection();
+
+app.Urls.Add("http://0.0.0.0:80");
 
 using (IServiceScope scope = app.Services.CreateScope())
 {
